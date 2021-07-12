df3ca950fd8b6ed5ecc83462e0ae162d443b077e
delaquash <olaidemmauel1@gmail.com>
2021-07-12 15:33:21 +0100

    fixed modules

---
+++ src/component/Quotes.js
@@ -0,0 +1,36 @@
+
+import React from 'react'
+
+ const Quotes =() =>{
+    return (
+        <div>
+            <div id="quote-box">
+                <div id="text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum error molestiae commodi, blanditiis temporibus nisi amet labore mollitia quis, enim quidem quaerat maiores hic eius necessitatibus animi debitis nemo voluptatem.
+                    Iusto maxime blanditiis dolorum laborum distinctio id maiores deserunt fugiat corporis quidem reprehenderit odio esse, modi ratione ipsum minima fuga qui fugit. Nostrum voluptatum velit ullam consectetur cupiditate. Pariatur, laudantium.
+                    Numquam repellat quo, eligendi vero dignissimos magni asperiores praesentium amet vel impedit, tenetur quibusdam unde. Perspiciatis aliquam earum explicabo, est nisi veniam rem itaque nihil sint doloremque ipsum pariatur voluptatibus?
+                    Adipisci, repellendus!</p>
+                </div>
+
+                <div id="buttons">
+                    <div className="social-media">
+                        <a href="#" id="tweet-quote">
+                            <span>
+                                <img src="" alt=""/>
+                            </span>
+                        </a>
+                        <a href="#" id="tweet-quote">
+                            <span>
+                                <img src="" alt=""/>
+                            </span>
+                        </a>
+
+                    </div>
+                    <button id="new-quote">New Quote</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Quotes;
