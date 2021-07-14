75e2e568a1a71246cd2f74ae243b6ef682bb3a40
delaquash <olaidemmauel1@gmail.com>
2021-07-14 16:52:45 +0100

    added axios to make API call

--- src/component/Quotes.js
+++ src/component/Quotes.js
@@ -5,14 +5,19 @@ import tumblr from '../tumblr.svg';
 import twitter from '../twitter.svg';
 
  const Quotes =() =>{
-    const [quote, setQuote] = useState('')
-    const [author, setAuthor] = useState('')
+    const [quote, setQuote] = useState('');
+    const [author, setAuthor] = useState('');
 
     useEffect(() => {
         const getQuotes = async () => {
-            const response = await axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
-            console.log(response.data);
-        }
+            const response = await axios
+                .get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+                    .then(res => res.json());
+                    .then(data => {
+                        let data = data.quotes;
+                        let randomNum = Math.floor(Math.random() * data.length);
+                    }) 
+        };
         getQuotes()
     }, [])
     return (
